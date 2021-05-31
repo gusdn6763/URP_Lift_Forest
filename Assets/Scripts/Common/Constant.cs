c943771c93c6a58be5905baae074559c9848886e
gusdn6763 <gusdn6763@gmail.com>
2021-05-31 17:29:54 +0900

    Add inventory

---
+++ Assets/Scripts/Common/Constant.cs
@@ -0,0 +1,17 @@
+﻿public class Constant
+{
+    public const string handRight = "Right";
+    public const string handLeft = "Left";
+
+    public const string player = "Player";
+    public const string weapon = "Weapon";
+    public const string fire = "Fire";
+
+    public const string attack = "Attack";
+    public const string move = "Move";
+    public const string idle = "Idle";
+    public const string speed = "Speed";
+
+    public const string item = "Item";
+}
+
---
+++ Assets/Scripts/InventorySocket.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+
+/// <summary>
+/// 첫번째 질문-> Socket에 오브젝트를 넣을시 OnSelectEntered함수를 실행하지 않음
+/// </summary>
+
+public class InventorySocket : XRSocketInteractor
+{
+    private SelectEnterEventArgs tmpArgs;
+    private Text myText;
+
+
+    public Item currentItem;
+
+    public int maxCount;
+    public int currentCount;
+
+    public int CurrentCount
+    {
+        get
+        {
+            return currentCount;
+        }
+        set
+        {
+            currentCount = value;
+            myText.text = currentCount.ToString();
+        }
+    }
+
+    protected override void Awake()
+    {
+        myText = GetComponentInChildren<Text>();
+        base.Awake();
+    }
+
+    protected override void Start()
+    {
+        CurrentCount = 0;
+        base.Start();
+    }
+
+
+    /// <summary>
+    /// 소켓에 어떠한 물체가 있으면 이 함수를 실행하지않음
+    /// </summary>
+    /// <param name="args"></param>
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        if (args.interactable.CompareTag(Constant.item))
+        {
+            tmpArgs = args;
+            Item tmp = args.interactable.GetComponent<Item>();
+            if (currentItem == null)
+            {
+                tmp.transform.SetParent(transform);
+                currentItem = tmp;
+            }
+            else
+            {
+                //열거형식 or 이런식
+                if (currentItem.GetType() == tmp.GetType())
+                {
+                    Destroy(tmp.gameObject);
+                }
+                else
+                {
+                    base.OnSelectEntered(args);
+                    return ;
+                }
+            }
+            CurrentCount++;
+        }
+        base.OnSelectEntered(args);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        CurrentCount -= 1;
+        print(currentItem);
+        if (CurrentCount > 0)
+        {
+            print(args.interactor);
+            print(args.interactable);
+            //Default오브젝트를 추가
+        }
+        else
+        {
+            currentItem = null;
+            tmpArgs = null;
+            base.OnSelectExited(args);
+        }
+    }
+ 
+
+    protected override void OnHoverEntered(HoverEnterEventArgs args)
+    {
+        if (tmpArgs != null)
+        {
+            OnSelectEntered(tmpArgs);
+        }
+        base.OnHoverEntered(args);
+    }
+}
