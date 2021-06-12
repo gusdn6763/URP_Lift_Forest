5e89ab394c0e87fe9853d2c8dbc064e4da5d5c39
gusdn6763 <gusdn6763@gmail.com>
2021-06-12 12:14:07 +0900

    add

--- Assets/Scripts/NPC/QuestNPC.cs
+++ Assets/Scripts/NPC/QuestNPC.cs
@@ -15,28 +15,44 @@ public class QuestNPC : NPC
     [SerializeField] private Item requestItem;                      //요구하는 퀘스트 아이템
     [SerializeField] private int requestItemCount;                  //요구하는 퀘스트 아이템 갯수
 
-
+    private FaceChange face;
     private int currentCount = 0;
-    private bool questAccept = false;               //퀘스트 수락상태
+    private bool questAccept = false;                               //퀘스트 수락상태
     private bool questComplete = false;                             //퀘스트 완료상태
+
+    protected override void Awake()
+    {
+        face = GetComponent<FaceChange>();
+        base.Awake();
+    }
 
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
-        base.OnSelectEntered(args);
         GetDialogue();
+        base.OnSelectEntered(args);
     }
 
+    /// <summary>
+    /// 특정 조건에 따라 NPC가 대화창을 띄우고 말하는 함수
+    /// </summary>
     public void GetDialogue( )
     {
+        //일정 거리에 있을시
         if ((Vector3.Distance(transform.position, Player.instance.transform.position) < interactiveRange))
         {
+            //NPC 대화창 활성화
             npcUI.gameObject.SetActive(true);
 
+            //퀘스트 수락한 적이 없을 경우
             if (!questAccept)
             {
+                //수락 비활성화 활성화 하고 버튼 텍스트 변경
                 npcUI.ButtonOnOff(true);
                 npcUI.ChangeButtonName("수락", "거절");
+                //대화창의 텍스트를 defaultDialogue로 변경
                 npcUI.ShowDialogue(this, defaultDialogue, defaultDialogueTime);
+
+                //왼쪽 버튼(수락)을 클릭시 실행
                 npcUI.SetOnClickAction(() =>
                 {
                     questAccept = true;
@@ -44,18 +60,21 @@ public class QuestNPC : NPC
                     npcUI.ButtonOnOff(false);
                 });
 
+                //오른쪽 버튼(수락)을 클릭시 실행
                 npcUI.SetNoClickAction(() =>
                 {
-                    npcUI.ShowDialogue(this, defaultDialogue, defaultDialogueTime);
-                    npcUI.gameObject.SetActive(false);
+                    npcUI.ShowDialogue(this, noAnswer, defaultDialogueTime);
+                    npcUI.ButtonOnOff(false);
                 });
             }
-            else
+            else            
             {
+                //퀘스트를 완료했었을 경우
                 if (questComplete)
                 {
                     npcUI.ShowDialogue(this, questCompleted, defaultDialogueTime);
                 }
+                //퀘스트를 수행중인 경우
                 else
                 {
                     npcUI.ShowDialogue(this, acceptAfterAnswer, defaultDialogueTime);
